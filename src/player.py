1e121eb86c6bd838d7dabbde6c85f444576ba145
Jayden Kanbour <z5316799@ad.unsw.edu.au>
2022-12-16 15:01:23 +1100

    creat music player ui

---
+++ src/player.py
@@ -0,0 +1,11 @@
+#importing libraries 
+from pygame import mixer
+from tkinter import *
+import tkinter.font as font
+from tkinter import filedialog
+
+#creating the root window 
+root=Tk()
+root.title('DataFlair Python MP3 Music player App ')
+#initialize mixer 
+mixer.init()
