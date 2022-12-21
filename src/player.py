994c12a2b2f29ae4ea52931d3242fcdfc1fb1d5d
Jayden Kanbour <jkanbour1@gmail.com>
2022-12-22 00:14:36 +1100

    simple functionality works

--- src/player.py
+++ src/player.py
@@ -4,6 +4,7 @@ from pygame import mixer
 from tkinter import *
 import tkinter.font as font
 from tkinter import filedialog
+import song
 
 #creating the root window 
 root=Tk()
@@ -19,32 +20,32 @@ songs_list.grid(columnspan=9)
 defined_font = font.Font(family='Helvetica')
 
 #play button
-play_button=Button(root,text="Play",width =7,command=print("play"))
+play_button=Button(root,text="Play",width =7,command=song.Play(songs_list))
 play_button['font']=defined_font
 play_button.grid(row=1,column=0)
 
 #pause button 
-pause_button=Button(root,text="Pause",width =7,command=print("pause"))
+pause_button=Button(root,text="Pause",width =7,command=song.Pause())
 pause_button['font']=defined_font
 pause_button.grid(row=1,column=1)
 
 #stop button
-stop_button=Button(root,text="Stop",width =7,command=print("stop"))
+stop_button=Button(root,text="Stop",width =7,command=song.Stop(songs_list))
 stop_button['font']=defined_font
 stop_button.grid(row=1,column=2)
 
 #resume button
-Resume_button=Button(root,text="Resume",width =7,command=print("resume"))
+Resume_button=Button(root,text="Resume",width =7,command=song.Resume())
 Resume_button['font']=defined_font
 Resume_button.grid(row=1,column=3)
 
 #previous button
-previous_button=Button(root,text="Prev",width =7,command=print("previous"))
+previous_button=Button(root,text="Prev",width =7,command=song.Previous(songs_list))
 previous_button['font']=defined_font
 previous_button.grid(row=1,column=4)
 
 #nextbutton
-next_button=Button(root,text="Next",width =7,command=print("next"))
+next_button=Button(root,text="Next",width =7,command=song.Next(songs_list))
 next_button['font']=defined_font
 next_button.grid(row=1,column=5)
 
@@ -53,8 +54,8 @@ my_menu=Menu(root)
 root.config(menu=my_menu)
 add_song_menu=Menu(my_menu)
 my_menu.add_cascade(label="Menu",menu=add_song_menu)
-add_song_menu.add_command(label="Add songs",command=print("add song"))
-add_song_menu.add_command(label="Delete song",command=print("delete song"))
+add_song_menu.add_command(label="Add songs",command=song.addsongs(songs_list))
+add_song_menu.add_command(label="Delete song",command=song.deletesong(songs_list))
 
 
 mainloop()
